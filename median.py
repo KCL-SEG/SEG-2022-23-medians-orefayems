d6ff13957d2d11ac948ed3f9b59a6caf9980f271
orefayems <62698264+orefayems@users.noreply.github.com>
2022-10-13 12:48:11 +0100

    Update median.py

--- median.py
+++ median.py
@@ -13,7 +13,9 @@ while True:
 sortedNumbers = sorted(numbers)
 n = numbers.size()/2
 
-if numbers.size()%2 != 0:
+if len(numbers) = 1:
+    print(numbers)
+ifel len(numbers)%2 != 0:
     print(sortedNumbers[n])
 else:
     print(sortedNumbers[sum(n-1,n)/2])
