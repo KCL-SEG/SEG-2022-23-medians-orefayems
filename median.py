cd78d468ba71be7644a3f553f957f7890e837aca
orefayems <62698264+orefayems@users.noreply.github.com>
2022-10-14 21:22:56 +0100

    Update median.py

--- median.py
+++ median.py
@@ -1,23 +1,29 @@
-"""Median calculator."""
+""""Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
+       	numbers = sorted([22.0, 23])
+        #numbers = sorted([float(value) for value in input().split(",")])
+        #let n be numberOfElements 
+        n = len(numbers)
+        if n%2 != 0:
+        	if n == 1:
+        		print(numbers)
+        		
+        	else:
+        		print(numbers[round(n/2)])
+        		
+        else:
+        	x1 = round((n/2)-1)
+        	x2= round(n/2)
+        	print((numbers[x1] + numbers[x2])/2)
+        	
+        
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
         
-sortedNumbers = sorted(numbers)
-n = len(numbers)/2
-
-if len(numbers) == 1:
-    print(numbers)
-elif len(numbers)%2 != 0:
-    print(sortedNumbers[n])
-else:
-    print(sortedNumbers[sum(n-1,n)/2])
-
-
+print(numbers)
